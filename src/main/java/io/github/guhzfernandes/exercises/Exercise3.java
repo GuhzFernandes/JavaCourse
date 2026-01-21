5f2a058372fec20693b9b1ac4c5379a8ba3f805d
Gustavo Fernandes <gustavofernandes330@gmail.com>
2026-01-21 05:32:47 -0300

    Exercise 3 solved

---
+++ src/main/java/io/github/guhzfernandes/exercises/Exercise3.java
@@ -0,0 +1,21 @@
+package io.github.guhzfernandes.exercises;
+// write a java program that compute the sum of digits of a number
+
+import java.util.Scanner;
+
+public class Exercise3 {
+    public void main() {
+        Scanner input = new Scanner(System.in);
+        int originalNumber, number, sum;
+
+        System.out.println("Enter a number to get the sum of it's digits");
+        originalNumber = number = input.nextInt();
+
+        sum=0;
+        while(number>0){
+            sum += number%10;
+            number /=10;
+        }
+        System.out.println("number:"+originalNumber+"\nsum: "+sum);
+    }
+}
