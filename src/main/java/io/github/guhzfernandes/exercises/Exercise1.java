a12ec1ee02fb092f5618c4eb53e4a10b12fa3919
Gustavo Fernandes <gustavofernandes330@gmail.com>
2026-01-24 20:27:58 -0300

    Exercise 1 solved

---
+++ src/main/java/io/github/guhzfernandes/exercises/Exercise1.java
@@ -0,0 +1,21 @@
+package io.github.guhzfernandes.exercises;
+//write a java program that allows the user to enter 10 number and give their sum.
+
+import java.util.Scanner;
+
+public class Exercise1 {
+    public void main() {
+
+        Scanner input = new Scanner(System.in);
+        int[] inputs = new int[10];
+        int sum = 0;
+
+        for(int i = 0;i<inputs.length;i++){
+            System.out.println("Insert the #"+(i+1)+" number:");
+            inputs[i] = input.nextInt();
+            sum+=inputs[i];
+        }
+        System.out.println("Sum: "+sum);
+        input.close();
+    }
+}
