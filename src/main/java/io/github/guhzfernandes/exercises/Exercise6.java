558284c5204c484e2bd5c2b64b8572b7fe16d3a7
Gustavo Fernandes <gustavofernandes330@gmail.com>
2026-01-24 22:54:54 -0300

    Exercise 6 solved

---
+++ src/main/java/io/github/guhzfernandes/exercises/Exercise6.java
@@ -0,0 +1,14 @@
+package io.github.guhzfernandes.exercises;
+//Write a java program that reverses an array
+
+public class Exercise6 {
+    public void main() {
+        int[] array = {1,2,3,4,5,6,7,8,9};
+        int[] reversedArray = new int[array.length];
+
+        for(int i=0; i<array.length; i++) reversedArray[array.length-1-i] = array[i];
+
+        for (int item:reversedArray) System.out.println(item);
+
+    }
+}
