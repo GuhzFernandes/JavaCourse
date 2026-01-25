c8dc516c77a43c426c83714a58abcf5351788a53
Gustavo Fernandes <gustavofernandes330@gmail.com>
2026-01-25 03:34:07 -0300

    Exercise 10 solved

---
+++ src/main/java/io/github/guhzfernandes/exercises/Exercise10.java
@@ -0,0 +1,14 @@
+package io.github.guhzfernandes.exercises;
+// Write a java program that reverses an entered string
+
+import java.util.Scanner;
+
+public class Exercise10 {
+    public void main() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("type anything");
+        System.out.println(new StringBuilder(input.nextLine()).reverse());
+        input.close();
+
+    }
+}
