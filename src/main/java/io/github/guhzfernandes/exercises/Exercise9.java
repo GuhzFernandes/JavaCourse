237478fe1bd04758e8857a8247dd6aa84eb8c49f
Gustavo Fernandes <gustavofernandes330@gmail.com>
2026-01-25 03:21:41 -0300

    Exercise 9 solution improved

--- src/main/java/io/github/guhzfernandes/exercises/Exercise9.java
+++ src/main/java/io/github/guhzfernandes/exercises/Exercise9.java
@@ -1,15 +1,28 @@
 package io.github.guhzfernandes.exercises;
-// Write a Java program that counts the letters, spaces, numbers and others caracteres of an input string.
+// Write a Java program that counts the letters, spaces, numbers and others characters of an input string.
 
 import java.util.Scanner;
 
 public class Exercise9 {
     public void main() {
         Scanner input = new Scanner(System.in);
+        int characters, numbers, spaces, others;
+        characters = numbers = spaces = others = 0;
 
         System.out.println("type anything:");
-        System.out.println(input.nextLine().length());
+        String inputText = input.nextLine();
+        for (int i = 0; i<inputText.length(); i++){
+            if(Character.isAlphabetic(inputText.charAt(i))) characters++;
+            else if (Character.isDigit(inputText.charAt(i))) numbers++;
+            else if (Character.isSpaceChar(inputText.charAt(i))) spaces++;
+            else others++;
+        }
 
+        System.out.println(inputText.length() + " total characters");
+        System.out.println(characters+" letters");
+        System.out.println(numbers+" numbers");
+        System.out.println(spaces+" spaces");
+        System.out.println(others+" others");
         input.close();
     }
 }
